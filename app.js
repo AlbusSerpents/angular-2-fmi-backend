b22f3c7a2a2d301ab7c2106b85a46578adb2a2e3
D.Naydenov <d.naydenov@futurist-labs.com>
2019-06-23 01:24:20 +0300

    Fixed security, and initialized invironments

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express');
 const security = require('./src/config/security');
 const securityRouter = require('express').Router();
@@ -5,7 +7,14 @@ const securityRouter = require('express').Router();
 const app = express();
 
 app.use(express.json());
-securityRouter.all('*', security.filter);
+securityRouter.all('/problems', security.filter);
+securityRouter.all('/problems/.*', security.filter);
+securityRouter.all('/users/.*', security.filter);
+securityRouter.all('/users', security.filter);
+securityRouter.all('/competitions/.*', security.filter);
+securityRouter.all('/competitions', security.filter);
+securityRouter.all('/logout', security.filter);
+
 app.use(securityRouter);
 
 require('./src/rest/registration.endpoint')(app);
@@ -14,6 +23,7 @@ require('./src/rest/users.endpoint')(securityRouter);
 require('./src/rest/problems.endpoint')(securityRouter);
 require('./src/rest/solutions.endpoint')(securityRouter);
 require('./src/rest/competitions.endpoint')(securityRouter);
+require('./src/rest/paritcipation.endpoint')(securityRouter);
 
 app.get('/status', (req, res) => {
   const status = {
