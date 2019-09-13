7d2968f64a4016f0a520833c27df3c35ff6e86c4
D.Naydenov <d.naydenov@futurist-labs.com>
2019-09-13 11:59:55 +0300

    Integrated with the AWS lambda for the interpreter

--- src/rest/code.endpoint.js
+++ src/rest/code.endpoint.js
@@ -2,8 +2,8 @@ const code = require('./../services/code.service');
 const networking = require('./../shared/networking.utils');
 
 module.exports = (secured) => {
-    secured.post('/run', (req, res) => {
-        const result = code.run(req.body);
+    secured.post('/run', async (req, res) => {
+        const result = await code.run(req.body);
         networking.makeResponse(result, res, 200);
     })
 }
--- src/services/code.service.js
+++ src/services/code.service.js
@@ -1,10 +1,14 @@
+const request = require('request-promise');
 const errorCodes = require('./../shared/error.codes');
 const interpreter = require('../interpreter/interpreter.facade');
 
-exports.run = (body) => {
-    if (body.code && body.input) {
-        return interpreter.interpret(body);
-    } else {
-        return { error: errorCodes.MISSING_FIELD };
-    }
+exports.run = async (body) => {
+    const requestOptions = {
+        body: body,
+        json: true,
+        method: 'POST',
+        uri: 'https://okwqdg5u7c.execute-api.us-east-1.amazonaws.com/prod/node-test',
+    };
+
+    return await request(requestOptions);
 }
